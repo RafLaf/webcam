78c765c97d6214a046d5443b04394953b88c4611
Antoine LAVRARD-MEYER <a19lavra@pc-elec-386.priv.enst-bretagne.fr>
2022-11-18 16:29:42 +0100

    missing add

--- cifar_10_benchmark.py
+++ cifar_10_benchmark.py
@@ -42,26 +42,34 @@ def get_loader():
     return trainset, testset, classes
 
 
-def create_data(trainset, shot_number=1, num_class=10):
+def create_data(trainset, shot_number=1, num_class=10,mean_computing_type="SNAPSHOT",data_aug=None):
     """
     create a few_shot data
     """
     data_fewshot = DataFewShot(num_class)
-    number_sample = np.zeros(num_class)
+    number_sample = np.zeros(num_class,dtype=np.int)
     total_sample = len(trainset)
     iteration = 0
-    
+    print("total sample in training :", total_sample)
 
-    while not (np.all(number_sample == shot_number)) and iteration < total_sample:
+    while  (iteration < total_sample):#and (np.any(number_sample < shot_number))
         img, classe = trainset[iteration]
         if number_sample[classe] < shot_number:
-            print(f"adding one sample to class {classe}")
-            representation = few_shot_model.get_features(img)
+            
+            representation = few_shot_model.get_features(img,augmentation=data_aug)
             data_fewshot.add_repr(classe, representation)
-            data_fewshot.add_mean_repr(representation, DEVICE)
+            
             number_sample[classe] += 1
+            if mean_computing_type=="SNAPSHOT":
+                data_fewshot.add_mean_repr(representation)
+        if mean_computing_type=="ALL":
+            data_fewshot.add_mean_repr(representation)
+
         iteration += 1
-    data_fewshot.aggregate_mean_rep()
+    print("number of image for computing mean : ",len(data_fewshot.mean_features))
+    print("number of sample per class",number_sample)
+    data_fewshot.aggregate_mean_rep(DEVICE)
+    #print("mean repr",data_fewshot.mean_features)
     return data_fewshot
 
 
@@ -76,9 +84,7 @@ def get_performance(dataset, data_fewshot):
         if i == 1000:
             break
 
-        classe_pred, prediction = few_shot_model.predict_class(
-            img, data_fewshot, data_fewshot.mean_features
-        )
+        classe_pred, prediction = few_shot_model.predict_class(img, data_fewshot)
 
         list_pred.append(classe_pred)
         list_gt.append(gt_classe)
@@ -87,10 +93,18 @@ def get_performance(dataset, data_fewshot):
             correct_pred += 1
         else:
             incorect_pred += 1
+
     return correct_pred / (correct_pred + incorect_pred), list_pred, list_gt
 
-
+torch.manual_seed(0)
 TRAIN, TEST, CLASSES = get_loader()
+
+
+
+
+
+# model parameters
+
 
 BACKBONE_SPECS = {
     "model_name": "resnet12",
@@ -104,15 +118,16 @@ BACKBONE_SPECS = {
     },
 }
 
-
-
-# model parameters
-CLASSIFIER_SPECS = {"model_name": "knn", "kwargs": {"number_neighboors": 1}}
+CLASSIFIER_SPECS = {
+    "model_name": "ncm", 
+    "kwargs":{}# {"number_neighboors": 1}
+}
 DEVICE = "cuda:0"
+
 #TRANSFORMS = get_camera_preprocess()
+
 TRANSFORMS = transforms.Compose(
         [transforms.ToTensor(),
-        
         transforms.Normalize(
             (0.485, 0.456, 0.406),
             (0.229, 0.224, 0.225)# np.array([x / 255.0 for x in [63.0, 62.1, 66.7]]),
@@ -122,13 +137,14 @@ TRANSFORMS = transforms.Compose(
         ])
 
 # number to save
-NUMBER_SHOT = 10
+NUMBER_SHOT = 5
 
 
 few_shot_model = FewShotModel(BACKBONE_SPECS, CLASSIFIER_SPECS, TRANSFORMS, DEVICE)
 # model related
 
-data = create_data(TRAIN, shot_number=NUMBER_SHOT)
+sub_train=torch.utils.data.Subset(TRAIN,[i for i in range(100)])
 
-final_perf, _,_ = get_performance(TEST, data)#final_list_pred, final_list_gt
+data = create_data(TRAIN, shot_number=NUMBER_SHOT,mean_computing_type="SNAPSHOT")
+final_perf, a,b = get_performance(TEST, data)#final_list_pred, final_list_gt
 print(final_perf)
